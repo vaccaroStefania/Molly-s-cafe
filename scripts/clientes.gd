b1f87f3916e420596d09a3e5cf5ceecba771dbaf
Stefanía Vaccaro <vaccarostefania49@gmail.com>
2026-08-06 00:48:42 -0300

    complicaciones para crear clientes por mi mala base de poo qwq

---
+++ scripts/clientes.gd
@@ -0,0 +1,39 @@
+extends Node2D
+class_name Cliente
+
+var nombre:String=""
+var dialogos:Array=["Buen día"]
+
+#estados
+var en_movimiento:bool=false
+var pedido_completo:bool=false
+var cliente_presente:bool=false
+#destinos
+var entrada_x=-280
+var pedido_x=250
+var ida_x=800
+#destino actual
+var destino_select: float = 250.0
+#capacidad
+var velocidad: float = 200.0
+
+func cambio_destino():
+	match destino_select:
+		250:
+			destino_select = ida_x
+			en_movimiento = true
+		800:
+			position.x = entrada_x   # Lo teletransporto a -280 
+			destino_select = pedido_x
+			en_movimiento = true
+	
+func _process(delta: float) -> void:
+	if en_movimiento:
+		#desde donde comienza, hasta donde va y su velocidad
+		position.x = move_toward(position.x, destino_select, velocidad * delta)
+		
+		if position.x == destino_select:
+			en_movimiento = false
+			
+			if destino_select == pedido_x and pedido_completo:
+				cambio_destino()
